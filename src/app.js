5bcd67d3553a72f071dacd315560dcfd9bef8be3
Gabriel-AFiori <gabriel_afiori@hotmail.com>
2024-11-19 18:47:05 -0300

    removendo port e listen desnecessarios do app.js e alterando minha config do vercel

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const fileRoutes = require('./routes/fileRoutes');
@@ -8,11 +8,12 @@ const errorMiddleware = require('./middlewares/errorMiddleware');
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3001;
 const app = express();
 
+// const PORT = process.env.PORT || 3001;
+
 const corsOptions = {
-  origin: 'https://jp-project-f.vercel.app',
+  origin: ['http://localhost:5173', 'https://jp-project-b.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
@@ -30,6 +31,6 @@ app.use('/upload', fileRoutes);
 // Adicionar um Middleware para tratamento de erros
 app.use(errorMiddleware);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
