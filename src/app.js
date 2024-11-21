4ba36e16bd34094e8e59bdea3df8bcbdab716880
Gabriel-AFiori <gabriel_afiori@hotmail.com>
2024-11-21 14:19:27 -0300

    Alterando o cors options e adicionando um novo header

--- src/app.js
+++ src/app.js
@@ -10,12 +10,11 @@ dotenv.config();
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://jp-project-f.vercel.app'); // Permite apenas esse domínio
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+app.use(cors({
+  origin: ['https://jp-project-f.vercel.app', 'http://localhost:5173'],
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'multipart/form-data'],
+}));
 
 app.use(bodyParser.json());
 
